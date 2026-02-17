6945fcc8c2e9da23b7022a93b1c4e4f5d350f2c0
yagelnikYael <yael0583228252@gmail.com>
2026-02-17 21:05:12 +0200

    Update fetch URL to use relative path for user API

--- client/src/components/features/login/Register.jsx
+++ client/src/components/features/login/Register.jsx
@@ -38,7 +38,7 @@ function Register({ open, handleClose, onOpenLogin }) {
         }
 
         try {
-            const res = await fetch("http://localhost:4000/user", {
+            const res = await fetch("/api/user", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username, password })
