d49f0fc7e6dec40fc49a4a66be002fb88e51e58e
yagelnikYael <yael0583228252@gmail.com>
2026-02-17 20:21:44 +0200

    Update API endpoint for deleting a product

--- client/src/components/features/product/ProductSlice.jsx
+++ client/src/components/features/product/ProductSlice.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export const deleteProduct = createAsyncThunk(
   "products/deleteProduct",
   async (id) => {
-    await axios.delete(`http://localhost:4000/product/${id}`);
+    await axios.delete(`/api/product/${id}`);
     return id; 
   }
 );
